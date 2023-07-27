4dfbe6d77341acc68519b1a2b517ed1c0874610c
gokay <gokayacikgoz1@gmail.com>
2023-07-27 13:15:21 +0300

    admin theme added

--- Casgem_MongoDb_Consume/Program.cs
+++ Casgem_MongoDb_Consume/Program.cs
@@ -27,4 +27,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+app.MapControllerRoute(
+  name: "areas",
+  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+
 app.Run();
